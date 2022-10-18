8bb7d185d8e146221fe8bca1cad1158482ac0193
lanomade <ganne.hannah@gmail.com>
2022-10-18 23:38:41 +0200

    create styled components for CreateEmployee

--- src/styles/CreateEmployee.js
+++ src/styles/CreateEmployee.js
@@ -2,6 +2,6 @@ import styled from "styled-components";
 
 export const FieldSet = styled.fieldset`
     padding: 1.5rem 2rem;
-    border-color: #f2f2f2;
+    border: 1px solid lightgray;
     border-radius: 2rem;
 `

1a269f2dc536f2f9b0bf2967bd5828e59453c45e
lanomade <ganne.hannah@gmail.com>
2022-10-18 20:26:57 +0200

    add react routes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import CreateEmployee from './components/CreateEmployee'
+import ViewEmployees from './components/ViewEmployees'
+import { store } from './utils/redux/store'
+import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route path="create-employee" element={<CreateEmployee />} />
+            <Route path="view-employees" element={<ViewEmployees /> } />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );

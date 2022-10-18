971eee274c78ad07ac4f7e3e5ba4e6fe3a343c56
lanomade <ganne.hannah@gmail.com>
2022-10-18 23:39:38 +0200

    create styled components for Table

---
+++ src/styles/Table.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const TableContainer = styled.table`
+    margin: 2rem 0;
+    width: 100%;
+    text-align: center;
+    border-spacing: 0;
+`
+
+export const TableHead = styled.thead`
+    background-color: var(--dark-gray);
+    color: var(--white);
+`
+
+export const TableRow = styled.tr`
+
+`
+
+export const TableHeader = styled.th`
+    padding: 0.8rem 1rem;
+
+`
+
+export const TableData = styled.td`
+    padding: 0.8rem 1rem;
+    border-bottom: 1px solid lightgray;
+`
+
+export const TableBody = styled.tbody`
+
+`

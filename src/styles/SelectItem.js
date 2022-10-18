edd3420f337ffc371d5a87d4bb635235a81b1d3a
lanomade <ganne.hannah@gmail.com>
2022-10-18 23:39:25 +0200

    add border bottom

--- src/styles/SelectItem.js
+++ src/styles/SelectItem.js
@@ -2,7 +2,7 @@ import styled from "styled-components"
 import { Input } from './InputItem'
 
 export const Select = styled(Input)`
-
+    border-bottom: 1px solid var(--dark-gray-blue);
 `
 
 export const Option = styled.option`

c4ca3f3af717d12f2f649d06205d7ade84c76e0d
lanomade <ganne.hannah@gmail.com>
2022-10-18 20:27:36 +0200

    create Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,37 @@
+import { HeaderContainer, HeaderLogo, Nav, NavLi, NavA, SubHeader } from '../styles/Header'
+import { Ul, A } from '../styles/GlobalStyle'
+import { useLocation } from 'react-router'
+
+export default function Header() {
+    const location = useLocation();
+
+    return (
+        <>
+            <HeaderContainer>
+            <HeaderLogo>
+                <A href="#">
+                    HRnet
+                </A>
+            </HeaderLogo>
+            <Nav>
+                <Ul>
+                    <NavLi>
+                        <NavA href="create-employee">
+                            Create Employee
+                        </NavA>
+                    </NavLi>
+                    <NavLi>
+                        <NavA href="view-employees" >
+                            View Current Employees
+                        </NavA>
+                    </NavLi>
+                </Ul>
+            </Nav>
+            </HeaderContainer>
+            <SubHeader>
+                <h2>{location.pathname === "/create-employee" ? 'Create Employee' : 'View Current Employees'}</h2>
+            </SubHeader>
+        </>
+        
+    )
+}

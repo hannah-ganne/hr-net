2d2093cd36e4e44ec7e91fea0d9ae5f7ee5b0c0d
lanomade <ganne.hannah@gmail.com>
2022-10-18 20:27:54 +0200

    create Select Item component

---
+++ src/components/SelectItem.jsx
@@ -0,0 +1,14 @@
+import { InputContainer, Label } from "../styles/InputItem";
+import { Select, Option } from "../styles/SelectItem";
+
+export default function SelectItem({ label, list, value, setValue }) {
+    return (
+        <InputContainer>
+            <Label>{label}</Label>
+            <Select as="select" onChange={(e) => setValue(e.target.value)}>
+                <Option>select your department</Option>
+                {list.map(item => <option key={item.id} value={item.name}>{item.name}</option>)}
+            </Select>
+        </InputContainer>
+    )
+}

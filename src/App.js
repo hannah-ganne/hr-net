7bffc68a471d26b5566ee52c0274b94c08990504
lanomade <ganne.hannah@gmail.com>
2022-10-18 20:26:43 +0200

    render header and add outlet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import GlobalStyle from './styles/GlobalStyle'
+import Header from './components/Header'
+import { Outlet } from 'react-router-dom';
+import { useState } from 'react'
+
 function App() {
 
-
+    return (
+        <>
+            <GlobalStyle />
+            <Header />
+            <Outlet />
+        </>
+    )
 }
 
 export default App;

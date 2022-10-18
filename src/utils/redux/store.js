65241731156858757f1bd5f89593de05ef4d9ceb
lanomade <ganne.hannah@gmail.com>
2022-10-18 23:41:53 +0200

    remove store

--- src/utils/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import employeeReducer from './employeeSlice'
-
-export const store = configureStore({
-    reducer: {
-        employee: employeeReducer,
-    }
-})

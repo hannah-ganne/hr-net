9e9b1088470c9909bd32225ac85f06071ff04840
lanomade <ganne.hannah@gmail.com>
2022-10-18 20:27:04 +0200

    create department list

---
+++ src/assets/data/departments.js
@@ -0,0 +1,9 @@
+const department = [
+    { id: 1, name: 'Sales' },
+    { id: 2, name: 'Marketing' },
+    { id: 3, name: 'Engineering' },
+    { id: 4, name: 'Human Resources' },
+    { id: 5, name: 'Legal' }
+]
+
+export default department

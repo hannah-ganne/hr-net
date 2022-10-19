e7d1d863b17f656762bc4cf5b659351656fc0b33
lanomade <ganne.hannah@gmail.com>
2022-10-19 21:30:47 +0200

    add sort and search features

--- src/components/ViewEmployees.jsx
+++ src/components/ViewEmployees.jsx
@@ -2,12 +2,24 @@ import { Main } from '../styles/GlobalStyle'
 import { EntryNum, Wrapper, SearchInput, RowWrapper } from '../styles/ViewEmployees'
 import Table from './Table'
 import Pagination from './Pagination'
-import { useState, useMemo } from 'react'
+import { useState, useMemo, useEffect } from 'react'
 
 export default function ViewEmployees() {
+    const employees = JSON.parse(localStorage.getItem('employees'))
+
+    // sort by column
+    const [column, setColumn] = useState('')
+    const [sortOrder, setSortOrder] = useState('')
+    const [sortedData, setSortedData] = useState([...employees])
+
+    const handleClick = (e) => {
+        setSortOrder(prev => (prev === '' || prev === 'desc') ? 'asc' : 'desc' )
+        setColumn(e.target.abbr)
+    }
+
+    // pagination
     const [currentPage, setCurrentPage] = useState(1)
     const [pageSize, setPageSize] = useState(10)
-    const employees = JSON.parse(localStorage.getItem('employees'))
 
     const currentTableData = useMemo(() => {
         const firstPageIndex = (currentPage - 1) * pageSize;
@@ -15,16 +27,32 @@ export default function ViewEmployees() {
         if (employees === null) {
             return []
         } else {
-            return employees.slice(firstPageIndex, lastPageIndex);
+            const sliced = [...sortedData]
+            return sliced.slice(firstPageIndex, lastPageIndex);
         }
-    }, [currentPage, pageSize]);
+    }, [currentPage, pageSize, sortedData]) 
 
     const firstEntry = 1 + ((currentPage - 1) * pageSize)
     const lastEntry = firstEntry + currentTableData.length -1
     const totalEntries = employees === null ? 0 : employees.length
-
-    console.log(employees)
     
+    // Search by keyword
+    const [keyword, setKeyword] = useState('')
+    const handleSearch = (e) => {
+        setKeyword(e.target.value)
+    }
+    
+    useEffect(() => {
+        const filtered = employees.filter(employee => Object
+            .keys(employee)
+            .some(key => employee[key]
+                .toLowerCase()
+                .includes(keyword.toLowerCase()
+            )))
+
+        setSortedData(filtered)
+    }, [keyword])
+
     return (
         <Main>
             <RowWrapper>
@@ -40,10 +68,10 @@ export default function ViewEmployees() {
                 </Wrapper>
                 <Wrapper>
                     Search
-                    <SearchInput />
+                    <SearchInput value={keyword} onChange={handleSearch}/>
                 </Wrapper>
             </RowWrapper>
-            <Table data={currentTableData} />
+            <Table data={currentTableData} column={column} setColumn={setColumn} sortOrder={sortOrder} setSortOrder={setSortOrder} handleClick={handleClick} setSortedData={setSortedData} />
             <RowWrapper>
                 <Wrapper>
                     Showing {firstEntry} to {lastEntry}  of {totalEntries} entries

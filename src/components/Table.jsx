425aa1223a78fc0d035aec4213a85efb2416e4d6
lanomade <ganne.hannah@gmail.com>
2022-10-19 15:35:04 +0200

    add sort feature

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,40 +1,137 @@
 import { TableContainer, TableHead, TableRow, TableHeader, TableData, TableBody } from '../styles/Table'
+import arrowUp from '../assets/images/arrow-up.svg'
+import { useState, useEffect } from 'react'
 
 export default function Table({ data }) {
+    const [column, setColumn] = useState('firstName')
+    const [sortOrder, setSortOrder] = useState('asc')
+    const [sortedData, setSortedData] = useState([])
 
+    useEffect(() => {
+        setSortedData([...data])
+    }, [data])
 
-    return (
-        <TableContainer>
-            <TableHead>
-            <TableRow>
-                <TableHeader>First Name</TableHeader>
-                <TableHeader>Last Name</TableHeader>
-                <TableHeader>Start Date</TableHeader>
-                <TableHeader>Department</TableHeader>
-                <TableHeader>Date of Birth</TableHeader>
-                <TableHeader>Street</TableHeader>
-                <TableHeader>City</TableHeader>
-                <TableHeader>State</TableHeader>
-                <TableHeader>Zip Code</TableHeader>
-            </TableRow>
-            </TableHead>
-            <TableBody>
-            {data.map((item, index) => {
-                return (
-                <TableRow key={`${item.lastName}-${index}`}>
-                    <TableData>{item.firstName}</TableData>
-                    <TableData>{item.lastName}</TableData>
-                    <TableData>{item.startDate}</TableData>
-                    <TableData>{item.department}</TableData>
-                    <TableData>{item.dateOfBirth}</TableData>
-                    <TableData>{item.address.street}</TableData>
-                    <TableData>{item.address.city}</TableData>
-                    <TableData>{item.address.state}</TableData>
-                    <TableData>{item.address.zipCode}</TableData>
-                </TableRow>
-                );
-            })}
-            </TableBody>
-        </TableContainer>
-    )
-}
+    const handleClick = (e) => {
+        setColumn(e.target.abbr)
+        setSortOrder(prev => prev === 'asc' ? 'desc' : 'asc')
+
+        if (sortOrder === 'asc') {
+            setSortedData(prev => prev.sort((a, b) => a[column].localeCompare(b[column])))
+        } else {
+            setSortedData(prev => prev.sort((a, b) => a[column].localeCompare(b[column])).reverse())
+        }
+    }
+
+    console.log(data)
+
+        return (
+            <TableContainer>
+                <TableHead>
+                    <TableRow>
+                        <TableHeader
+                            abbr="firstName"
+                            onClick={handleClick}
+                            sortOrder={sortOrder}
+                            column={column}
+                        >
+                            First Name
+                            <img src={arrowUp} />
+                        </TableHeader>
+                        <TableHeader 
+                            abbr="lastName" 
+                            onClick={handleClick} 
+                            sortOrder={sortOrder} 
+                            column={column}
+                        >
+                            Last Name
+                            <img src={arrowUp} />
+                        </TableHeader>
+                        <TableHeader 
+                            abbr="startDate" 
+                            onClick={handleClick} 
+                            sortOrder={sortOrder} 
+                            column={column}
+                        >
+                            Start Date
+                            <img src={arrowUp} />
+                        </TableHeader>
+                        <TableHeader 
+                            abbr="department" 
+                            onClick={handleClick} 
+                            sortOrder={sortOrder} 
+                            column={column}
+                        >
+                            Department
+                            <img src={arrowUp} />
+                        </TableHeader>
+                        <TableHeader 
+                            abbr="dateOfBirth" 
+                            onClick={handleClick} 
+                            sortOrder={sortOrder} 
+                            column={column}
+                        >
+                            Date of Birth
+                            <img src={arrowUp} />
+                        </TableHeader>
+                        <TableHeader 
+                            abbr="street" 
+                            onClick={handleClick} 
+                            sortOrder={sortOrder} 
+                            column={column}
+                        >
+                            Street
+                            <img src={arrowUp} />
+                        </TableHeader>
+                        <TableHeader 
+                            abbr="city" 
+                            onClick={handleClick} 
+                            sortOrder={sortOrder} 
+                            column={column}
+                        >
+                            City
+                            <img src={arrowUp} />
+                        </TableHeader>
+                        <TableHeader 
+                            abbr="state" 
+                            onClick={handleClick} 
+                            sortOrder={sortOrder} 
+                            column={column}
+                        >
+                            State
+                            <img src={arrowUp} />
+                        </TableHeader>
+                        <TableHeader 
+                            abbr="zipCode" 
+                            onClick={handleClick} 
+                            sortOrder={sortOrder} 
+                            column={column}
+                        >
+                            Zip Code
+                            <img src={arrowUp} />
+                        </TableHeader>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {sortedData.length === 0
+                        ? <TableRow>
+                            <TableData colSpan={9}>There is no registered employee</TableData>
+                        </TableRow>
+                        : sortedData.map((item, index) => {
+                            return (
+                                <TableRow key={`${item.lastName}-${index}`}>
+                                    <TableData>{item.firstName}</TableData>
+                                    <TableData>{item.lastName}</TableData>
+                                    <TableData>{item.startDate}</TableData>
+                                    <TableData>{item.department}</TableData>
+                                    <TableData>{item.dateOfBirth}</TableData>
+                                    <TableData>{item.street}</TableData>
+                                    <TableData>{item.city}</TableData>
+                                    <TableData>{item.state}</TableData>
+                                    <TableData>{item.zipCode}</TableData>
+                                </TableRow>
+                            );
+                        })}
+                </TableBody>
+            </TableContainer>
+        )
+    }

f640128d749604c73592bd92b340b9f8055c750f
lanomade <ganne.hannah@gmail.com>
2022-10-19 15:34:50 +0200

    add disabled feature for first and last page

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,12 +1,23 @@
 import { PaginationNav, NavButton, PageNum } from "../styles/Pagination"
 
-export default function Pagination({ onPageChange, currentPage }) {
+export default function Pagination({ onPageChange, currentPage, totalEntries, pageSize }) {
+    const numOfPage = Math.ceil(totalEntries / pageSize)
 
     return (
         <PaginationNav>
-            <NavButton onClick={() => onPageChange(currentPage => currentPage - 1)}>Previous</NavButton>
+            <NavButton
+                onClick={() => onPageChange(currentPage => currentPage - 1)}
+                disabled={currentPage === 1}
+            >
+                Previous
+            </NavButton>
             <PageNum>{currentPage}</PageNum>
-            <NavButton onClick={() => onPageChange(currentPage => currentPage + 1)}>Next</NavButton>
+            <NavButton
+                onClick={() => onPageChange(currentPage => currentPage + 1)}
+                disabled={currentPage === numOfPage}
+            >
+                Next
+            </NavButton>
         </PaginationNav>
     )
 }
